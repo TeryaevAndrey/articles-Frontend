832afaeb50c3e5250626823d083c60a4d2afd405
Andrey <teryaev.2004@mail.ru>
2023-04-10 21:18:25 +0300

    fix-header-Profile-component

--- src/components/Header/Profile/Profile.tsx
+++ src/components/Header/Profile/Profile.tsx
@@ -2,28 +2,31 @@ import React, { FC } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import ProfileMenu from "./ProfileMenu/ProfileMenu";
+import { useAppDispatch, useAppSelector } from "../../../store/store";
+import { setIsOpenMenu } from "../../../store/slices/headerSlice";
 
 const Profile: FC = () => {
-  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const isOpenMenu = useAppSelector((state) => state.header.isOpenMenu);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="relative">
       <div
         className="flex items-center gap-2 cursor-pointer"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => dispatch(setIsOpenMenu(!isOpenMenu))}
       >
         <div className="flex items-center gap-1.5">
           <div className="w-10 h-10 rounded-full bg-gray-300"></div>
           <div className="text-gray-600">username</div>
         </div>
         <AiFillCaretDown
-          className={`${isOpen && "rotate-180"}`}
+          className={`${isOpenMenu && "rotate-180"}`}
           size={10}
           color="#3b82f6"
         />
       </div>
 
-      {isOpen && <ProfileMenu />}
+      {isOpenMenu && <ProfileMenu />}
     </div>
   );
 };
