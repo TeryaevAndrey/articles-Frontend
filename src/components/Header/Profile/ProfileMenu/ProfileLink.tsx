db1f6e0d6b3b2bbc4570910bb9b588e8ba3903b1
Andrey <teryaev.2004@mail.ru>
2023-04-10 23:25:55 +0300

    close-is-open-menu-header-on-click-link

--- src/components/Header/Profile/ProfileMenu/ProfileLink.tsx
+++ src/components/Header/Profile/ProfileMenu/ProfileLink.tsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 interface IProfileLink {
   href: string;
   title: string;
+  onClick?: React.MouseEventHandler;
 }
 
-const ProfileLink: FC<IProfileLink> = ({ href, title }) => {
+const ProfileLink: FC<IProfileLink> = ({ href, title, onClick }) => {
   return (
     <Link
       className="p-2 border-b border-gray-300 border-solid w-full lg:hover:bg-gray-200 ease-linear duration-200 active:bg-gray-200"
       to={href}
+      onClick={onClick}
     >
       {title}
     </Link>
--- src/components/Header/Profile/ProfileMenu/ProfileMenu.tsx
+++ src/components/Header/Profile/ProfileMenu/ProfileMenu.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import ProfileLink from "./ProfileLink";
+import { setIsOpenMenu } from "../../../../store/slices/headerSlice";
+import { useAppDispatch } from "../../../../store/store";
 
 const ProfileMenu: FC = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="absolute top-[130%] flex flex-col w-full max-w-[150px] bg-slate-100 truncate rounded overflow-hidden text-sm z-10">
-      <ProfileLink title="Профиль" href="/profile/asdklasdk123" />
-      <ProfileLink title="Добавить статью" href="/add-article" />
-      <ProfileLink title="Избранное" href="/favourites" />
+      <ProfileLink title="Профиль" href="/profile/asdklasdk123" onClick={() => dispatch(setIsOpenMenu(false))} />
+      <ProfileLink title="Добавить статью" href="/add-article" onClick={() => dispatch(setIsOpenMenu(false))} />
+      <ProfileLink title="Избранное" href="/favourites" onClick={() => dispatch(setIsOpenMenu(false))} />
 
       <Link
         className="p-2 border-b border-gray-300 border-solid w-full text-red-400"
