2a10e5943259328531d67dea992eb6d0be175b9f
Andrey <teryaev.2004@mail.ru>
2023-04-10 21:07:41 +0300

    add-states-and-changes-functions-for-login-page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 const LoginPage: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="mt-10">
       <div className="container mx-auto px-4">
@@ -11,11 +22,15 @@ const LoginPage: FC = () => {
               className="px-3 py-2 rounded text-sm placeholder:text-sm placeholder:font-light"
               type="text"
               placeholder="Email..."
+              onChange={onEmailChange}
+              value={email}
             />
             <input
               className="px-3 py-2 rounded text-sm placeholder:text-sm placeholder:font-light"
               type="text"
               placeholder="Пароль..."
+              onChange={onPasswordChange}
+              value={password}
             />
           </div>
           <button className="w-full bg-blue-500 text-white rounded px-3 py-2 mt-5">
