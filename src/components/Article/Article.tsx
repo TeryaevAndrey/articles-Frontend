4cefd94463e7ad7c05bebb5a258bffe00b32395d
Andrey <teryaev.2004@mail.ru>
2023-04-27 16:29:27 +0300

    add-icon-(mark)-for-article

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -3,6 +3,7 @@ import { AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { IArticle } from "../../types";
 import Tag from "./Tag";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
 
 const Article: FC<IArticle> = ({ _id, title, banner, elements, tags, views, from, createdAt, updatedAt }) => {
   const text = elements.find((el) => el.type === "text");
@@ -40,12 +41,15 @@ const Article: FC<IArticle> = ({ _id, title, banner, elements, tags, views, from
               <AiOutlineEye size={15} />
               <p className="text-sm text-gray-300 font-light">{views} просмотров</p>
             </div>
-            <Link
-              className="px-2 py-1 text-sm bg-blue-500 text-white rounded"
-              to={`/articles/${_id}`}
-            >
-              Посетить
-            </Link>
+            <div className="flex items-center gap-3">
+              <BsBookmark className="text-blue-500 cursor-pointer" size={20} />
+              <Link
+                className="px-2 py-1 text-sm bg-blue-500 text-white rounded"
+                to={`/articles/${_id}`}
+              >
+                Посетить
+              </Link>
+            </div>
           </div>
         </div>
       </div>
